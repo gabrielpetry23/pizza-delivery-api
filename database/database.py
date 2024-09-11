02b6604ccffc57d62d1e5c1dbaa42faa63eed91d
gabrielpetry23 <gabriel.petry23@gmail.com>
2024-09-11 20:42:18 -0300

    bug fixed

--- database/database.py
+++ database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine('postgreesql://postgres:2308@localhost:5432/pizza_delivery',
+engine = create_engine('postgresql://postgres:2308@localhost:5432/pizza_delivery',
     echo=True
 )
 
--- database/models.py
+++ database/models.py
@@ -1,7 +1,7 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, Text, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlaclhemy_utils.types import ChoiceType
+from sqlalchemy_utils.types import ChoiceType
 
 class User(Base):
     __tablename__ = 'users'
@@ -10,11 +10,11 @@ class User(Base):
     email = Column(String(80), unique=True)
     password = Column(Text, nullable=True)
     is_active = Column(Boolean, default=False)
-    is_active = Column(Boolean, default=False)
+    is_staff = Column(Boolean, default=False)
     orders = relationship('Order', back_populates='user')
     
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.username}'
     
     
 class Order(Base):
@@ -38,8 +38,8 @@ class Order(Base):
     quantity = Column(Integer, nullable=False)
     order_status = Column(ChoiceType(choices=ORDER_STATUS), default='PENDING')
     pizza_size = Column(ChoiceType(choices=PIZZA_SIZE), default='SMALL')
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship('User', back_populates='orders')
     
     def __repr__(self):
-        return f'<Order {self.id}>'
+        return f'<Order {self.id}'

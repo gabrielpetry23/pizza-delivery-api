e27aaf485a6ba98a96272eaf2a2f37c69c86dee9
gabrielpetry23 <gabriel.petry23@gmail.com>
2024-09-18 20:24:39 -0300

    get user orders route

--- routes/orders.py
+++ routes/orders.py
@@ -70,7 +70,7 @@ async def create_order(order: OrderModel, Authorize: AuthJWT = Depends()):
     return jsonable_encoder(response)
 
 
-@order_router.get("/order/{id}")
+@order_router.get("/orders/{id}")
 async def get_order_by_id(id: int, Authorize: AuthJWT = Depends()):
     try:
         Authorize.jwt_required()
@@ -92,3 +92,18 @@ async def get_order_by_id(id: int, Authorize: AuthJWT = Depends()):
         return jsonable_encoder(order)
         
     raise HTTPException( status_code=status.HTTP_401_UNAUTHORIZED, detail="Only staff can view this page!")
+
+@order_router.get('/user/orders')
+async def get_user_orders(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+        )
+    
+    user = Authorize.get_jwt_subject()
+    current_user = session.query(User).filter(User.username == user).first()
+    
+    return jsonable_encoder(current_user.orders)
